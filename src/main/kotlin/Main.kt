70857b07bf8c0fddc6f9df0a177eff62841326a3
Winfida-Obinyo <obinyowinfridaa@gmail.com>
2023-04-06 21:36:45 +0300

    prints numbers 1 to 10

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main(){
+    numbers()
+    println()
+}
+//Write a program in Kotlin that prints the numbers from 1 to 10 using a for loop.
+fun numbers(){
+    for (i in 1..10){
+        println(i)
+    }
+}
